cd78e9a09b3011a117acf9bd24e7d12df27ed03f
Y-ostin <yostin.arequipa@tecsup.edu.pe>
2024-10-21 17:08:25 -0500

    feat: add dependencies of maps in graddle(modulde)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,6 @@ secrets {
 
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -82,4 +81,15 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    val mapsComposeVersion = "4.4.1"
+
+    // Google Maps Compose
+    implementation("com.google.maps.android:maps-compose:$mapsComposeVersion")
+
+    // Utilidades de Google Maps para Jetpack Compose
+    implementation("com.google.maps.android:maps-compose-utils:$mapsComposeVersion")
+
+    // Widgets de Google Maps Compose
+    implementation("com.google.maps.android:maps-compose-widgets:$mapsComposeVersion")
 }
