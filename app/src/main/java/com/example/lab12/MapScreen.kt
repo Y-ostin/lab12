850b44f815a04d72e9784910d6f30a05e773dcf9
Y-ostin <yostin.arequipa@tecsup.edu.pe>
2024-10-21 18:29:06 -0500

    feat:add poligons on on the map

--- app/src/main/java/com/example/lab12/MapScreen.kt
+++ app/src/main/java/com/example/lab12/MapScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
@@ -15,6 +16,7 @@ import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.Polygon
 
 
 @Composable
@@ -30,6 +32,7 @@ fun MapScreen() {
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
+
         ) {
             // Añadir marcador en Arequipa Perú
             val context = LocalContext.current
@@ -39,7 +42,7 @@ fun MapScreen() {
 
             Marker(
                 state = rememberMarkerState(position = ArequipaLocation),
-                icon = customMarker, // Usar el marcador escalado
+                 // Usar el marcador escalado
                 title = "Arequipa, Perú"
             )
             val locations = listOf(
@@ -63,9 +66,46 @@ fun MapScreen() {
                 )
             }
 
+            val mallAventuraPolygon = listOf(
+                LatLng(-16.432292, -71.509145),
+                LatLng(-16.432757, -71.509626),
+                LatLng(-16.433013, -71.509310),
+                LatLng(-16.432566, -71.508853)
+            )
 
 
+            val parqueLambramaniPolygon = listOf(
+                LatLng(-16.422704, -71.530830),
+                LatLng(-16.422920, -71.531340),
+                LatLng(-16.423264, -71.531110),
+                LatLng(-16.423050, -71.530600)
+            )
 
+            val plazaDeArmasPolygon = listOf(
+                LatLng(-16.398866, -71.536961),
+                LatLng(-16.398744, -71.536529),
+                LatLng(-16.399178, -71.536289),
+                LatLng(-16.399299, -71.536721)
+            )
+
+            Polygon(
+                points = plazaDeArmasPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
+            Polygon(
+                points = parqueLambramaniPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
+            Polygon(
+                points = mallAventuraPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f,
+                )
         }
     }
 }
